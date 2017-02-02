96463cd6a37d0f8077bcc5fd9a2b5cdb7bdb1d92
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-02-02 11:03:04 +0300

    -

--- lib/src/analytics_component.dart
+++ lib/src/analytics_component.dart
@@ -5,7 +5,7 @@ import 'package:alert/alert_service.dart';
 import 'package:js/js.dart';
 
 import 'package:resources_loader/resources_loader.dart';
-
+import 'package:chartjs/chartjs.dart';
 @Component(selector: 'analytics')
 @View(
     templateUrl: 'analytics_component.html',
@@ -43,5 +43,46 @@ class AnalyticsComponent implements OnInit {
 
     _resourcesLoaderService.loadScript('vendor/chart.js/dist/', 'Chart.min.js', false);
     _resourcesLoaderService.loadScript('packages/analytics/src/', 'init-analytics.js', false);
+  }
+
+
+  /**
+   *     var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+      var lineChartData = {
+      labels : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль'],
+      datasets : [
+      {
+      label: 'План 2016',
+      backgroundColor : 'rgba(220,220,220,0.2)',
+      borderColor : 'rgba(220,220,220,1)',
+      pointBackgroundColor : 'rgba(220,220,220,1)',
+      pointBorderColor : '#fff',
+      data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+      },
+      {
+      label: 'Факт 2016',
+      backgroundColor : 'rgba(151,187,205,0.2)',
+      borderColor : 'rgba(151,187,205,1)',
+      pointBackgroundColor : 'rgba(151,187,205,1)',
+      pointBorderColor : '#fff',
+      data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+      }
+      ]
+      }
+
+      var ctx = document.getElementById('canvas-1');
+      var chart = new Chart(ctx, {
+      type: 'line',
+      data: lineChartData,
+      options: {
+      responsive: true
+      }
+      });
+   */
+  /
+  void initChart1(){
+
+
+    new Chart(querySelector('#canvas-1') as CanvasElement, config);
   }
 }
